ed3c73fca0a29e6ee02143a53971af1c43ab5161
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2023-01-09 09:37:13 -0500

    added login handlebars

--- client/LoginForm.js
+++ client/LoginForm.js
@@ -1,14 +1,20 @@
 class LoginForm extends React.Component {
     handleSubmit = (event) => {
-      event.preventDefault();
-      // Handle form submission here
-    }
-  
-    render() {
-      return (
-        <form id="login-form" onSubmit={this.handleSubmit}>
-          {/* Form fields go here */}
-        </form>
-      );
-    }
+        event.preventDefault();
+      
+        const formData = new FormData(event.target);
+        const data = {};
+        formData.forEach((value, key) => {
+          data[key] = value;
+        });
+      
+        axios.post('/login', data)
+          .then((response) => {
+            // Process the response from the server
+          })
+          .catch((error) => {
+            // Handle any errors
+          });
+      }
+      
   }
