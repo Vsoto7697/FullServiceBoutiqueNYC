568b945ba5a2f6d3f76b6f086e5088e91b51f4d3
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2023-01-09 10:02:36 -0500

    added main handlebars

--- server/server.js
+++ server/server.js
@@ -3,9 +3,16 @@ const mongoose = require('mongoose');
 const handlebars = require('handlebars');
 const cors = require('cors');
 const routes = require('./routes');
+const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.render('template', {
+    loggedIn: req.session.user !== undefined,
+    body: '<h1>Welcome to the Full Service Boutique</h1>',
+  });
+});
 
-
+app.use('/', router);
 
 // Set Handlebars as the default templating engine.
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
