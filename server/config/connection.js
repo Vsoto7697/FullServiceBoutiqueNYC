886915855584c770bebb97ca46ff39888a8e547c
Daniel Papert <dpapert@gmail.com>
2023-01-10 20:53:54 -0500

    Backend works! Moving onto front end developmennt.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/full-service-botique',
+  process.env.MONGODB_URI || 'mongodb://localhost/full-service-botique',
   {
+    //127.0.0.1:27017
     useNewUrlParser: true, 
     useUnifiedTopology: true,
   }
--- server/server.js
+++ server/server.js
@@ -2,43 +2,21 @@ const path = require('path');
 require("dotenv").config();
 const express = require('express');
 const mongoose = require('mongoose');
-const handlebars = require('handlebars');
 const cors = require('cors');
-const routes = require('./routes');
 const router = express.Router();
 const {ApolloServer} = require('apollo-server-express');
 // Handlebars helpers
 const helpers = require('./utils/helpers');
-// Handlebars template engine for front-end
-const expressHandlebars = require('express-handlebars');
-// Initialize handlebars for the html templates
-const exphbs = expressHandlebars.create({helpers});
 const {typeDefs, resolvers} = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
-
-router.get('/', (req, res) => {
-  res.render('template', {
-    loggedIn: req.session.user !== undefined,
-    body: '<h1>Welcome to the Full Service Boutique</h1>',
-  });
-});
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/', router);
-// Set handlebars as the template engine for the server
-app.engine('handlebars', exphbs.engine);
-app.set('view engine', 'handlebars');
-app.use('/', router);
+
 // Enable CORS
 app.use(cors());
 app.get('*', (req, res) => {
@@ -52,14 +30,13 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// Create MongoDB
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/fullservicebotique', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
 });
-// Logs mono queries
-mongoose.set('debug', true);
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
@@ -76,12 +53,4 @@ const startApolloServer = async (typeDefs, resolvers) => {
   
   // Call the async function to start the server
   startApolloServer(typeDefs, resolvers);
-
-
-// Use the routes
-app.use('/', routes);
-
-app.listen(3001, () => {
-    console.log('Server is running on port 3001');
-  });
 
